0b41f4fe7ef1e461e156925543753b834846aa05
Ross Jacobs <rossjacobs@cloudflare.com>
2023-06-15 14:05:06 -0500

    Move content to tools.ross.gg/bulk_delete_notion_pages
    Also updated multiple API calls, as Notion API calls have changed over
    the years. I used Chrome's devtools to inspect the new calls.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Empty Notion Trash
 
-Visit [the webpage](https://pocc.github.io/bulk_delete_notion_pages) for this
+Visit [the webpage](https://tools.ross.gg/bulk_delete_notion_pages) for this
 repo and follow the instructions there.
--- index.js
+++ index.js
@@ -1,27 +1,67 @@
-javascript:(function(){
+javascript: (function () {
   // Author: Ross Jacobs
   // Purpose: Use as a browser bookmarklet to bulk delete notion pages in trash
   // License: Apache 2.0
   async function getSpaceId() {
-    resp = await fetch("https://www.notion.so/api/v3/loadUserContent", {"credentials":"include","headers":{"accept":"*/*","cache-control":"no-cache","content-type":"application/json","pragma":"no-cache","sec-fetch-mode":"cors","sec-fetch-site":"same-origin"},"referrerPolicy":"same-origin","body":"{}","method":"POST","mode":"cors"}); 
-    json = await resp.json(); 
-    spaceId = Object.keys(json.recordMap.space)[0]; 
+    resp = await fetch("https://www.notion.so/api/v3/loadUserContent", {
+      credentials: "include",
+      headers: {
+        accept: "*/*",
+        "cache-control": "no-cache",
+        "content-type": "application/json",
+        pragma: "no-cache",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+      },
+      referrerPolicy: "same-origin",
+      body: "{}",
+      method: "POST",
+      mode: "cors",
+    });
+    json = await resp.json();
+    spaceId = Object.keys(json.recordMap.space)[0];
     return spaceId;
-  }
+  };
 
   async function getBlockIds(spaceId) {
-    resp = await fetch("https://www.notion.so/api/v3/search",{"credentials":"include","headers":{"accept":"*/*","cache-control":"no-cache","content-type":"application/json","pragma":"no-cache","sec-fetch-mode":"cors","sec-fetch-site":"same-origin"},"referrerPolicy":"same-origin","body":"{\"type\":\"BlocksInSpace\",\"query\":\"\",\"filters\":{\"isDeletedOnly\":true,\"excludeTemplates\":false,\"isNavigableOnly\":true,\"requireEditPermissions\":false,\"ancestors\":[],\"createdBy\":[],\"editedBy\":[],\"lastEditedTime\":{},\"createdTime\":{}},\"sort\":\"Relevance\",\"limit\":1000,\"spaceId\":\"" + spaceId + "\",\"source\":\"trash\"}","method":"POST","mode":"cors"}); 
-    json = await resp.json(); 
-    blockIds = json.results.map((el) => {return el.id}); 
+    let resp = await fetch("https://www.notion.so/api/v3/search", {
+      headers: {
+        accept: "*/*",
+        "accept-language": "en-US,en;q=0.9",
+        "cache-control": "no-cache",
+        "content-type": "application/json",
+      },
+      body: `{\"type\":\"BlocksInSpace\",\"spaceId\":\"${spaceId}\",\"limit\":1000,\"filters\":{\"isDeletedOnly\":true,\"excludeTemplates\":false,\"navigableBlockContentOnly\":false,\"requireEditPermissions\":false,\"includePublicPagesWithoutExplicitAccess\":false,\"ancestors\":[],\"createdBy\":[],\"editedBy\":[],\"lastEditedTime\":{},\"createdTime\":{},\"inTeams\":[]},\"sort\":{\"field\":\"relevance\"},\"source\":\"quick_find_input_change\",\"searchExperimentOverrides\":{}}`,
+      method: "POST",
+      mode: "cors",
+      credentials: "include",
+    });
+    json = await resp.json();
+    blockIds = json.results.map((el) => {
+      return el.id;
+    });
     return blockIds;
   }
 
-  (async ()=>{
+  (async () => {
     const spaceId = await getSpaceId();
     blockIds = await getBlockIds(spaceId);
     for (const blockId of blockIds) {
-      const blockIdEscaped = '\"' + blockId + '\"';
-      await fetch("https://www.notion.so/api/v3/deleteBlocks", {"credentials":"include","headers":{"accept":"*/*","cache-control":"no-cache","content-type":"application/json","pragma":"no-cache","sec-fetch-mode":"cors","sec-fetch-site":"same-origin"},"referrerPolicy":"same-origin","body":"{\"blockIds\":[" +blockIdEscaped+"],\"permanentlyDelete\":true}","method":"POST","mode":"cors"});
+      await fetch("https://www.notion.so/api/v3/deleteBlocks", {
+        "headers": {
+          "accept": "*/*",
+          "accept-language": "en-US,en;q=0.9",
+          "cache-control": "no-cache",
+          "content-type": "application/json",
+        },
+        "referrer": "https://www.notion.so/qwerk/Lorem-Ipsum-d883cc4e6ea64bd4bcdb85c43cf74946",
+        "referrerPolicy": "strict-origin-when-cross-origin",
+        "body": `{\"blocks\":[{\"id\":\"${blockId}\",\"spaceId\":\"${spaceId}\"}],\"permanentlyDelete\":true}`,
+        "method": "POST",
+        "mode": "cors",
+        "credentials": "include"
+      });
     }
   })();
 })();
+
